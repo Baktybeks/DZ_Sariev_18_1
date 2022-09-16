270b80b382fa69115ade0c172b80a0d42f50aa49
Baktybeks <baktybeks@mail.ru>
2022-09-16 10:03:18 +0600

    DZ 3_1

--- DZ_3/zadanie_3_1.js
+++ DZ_3/zadanie_3_1.js
@@ -1,6 +1,6 @@
 const chessboardSize = prompt("Введите размер шахматного стола");
-let chessboard = "";
 for (let i = 1; i < Number(chessboardSize)+1; i++) {
+    let chessboard = "";
     for (let j = 1; j < Number(chessboardSize)+1; j++) {
         if ((i+j) % 2 === 0) {
             chessboard = chessboard.concat("#")
@@ -8,6 +8,8 @@ for (let i = 1; i < Number(chessboardSize)+1; i++) {
             chessboard = chessboard.concat(" ")
         }
     }
-    chessboard = chessboard.concat("\n")
+    console.log(chessboard)
 }
-alert(chessboard)
+
+
+
