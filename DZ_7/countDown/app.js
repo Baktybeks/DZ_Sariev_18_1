0e3175ec94d088483c6d598bfde6ed6ad53dc8f7
Baktybeks <baktybeks@mail.ru>
2022-09-28 09:39:26 +0600

    dz_7

--- DZ_7/countDown/app.js
+++ DZ_7/countDown/app.js
@@ -1,43 +1,52 @@
 const timeWrapper = document.getElementById("counterTimer");
-const startDate = new Date(2022, 9, 1, 20, 0, 0);
+const startDate = new Date(2022, 8, 1, 20, 0, 0); // 1 октября 2022
 
 const daysCounterElement = timeWrapper.querySelector("#timer-days-value");
 const hoursCounterElement = timeWrapper.querySelector("#timer-hours-value");
 const minutesCounterElement = timeWrapper.querySelector("#timer-minutes-value");
 const secondsCounterElement = timeWrapper.querySelector("#timer-seconds-value");
 
-
-
-
 function GetDiffDate(date) {
-    const dateNow = new Date();
-    return date - dateNow;
-
+  const dateNow = new Date();
+  return date - dateNow;
 }
 
-function ExtractDaysFromDiffDate(diffDate) {
-    return Math.floor(diffDate / (24 * 60 * 60 * 1000));
-}
+const ExtractDaysFromDiffDate = (diffDate) =>
+  Math.floor(diffDate / (24 * 60 * 60 * 1000));
 
-function ExtractHoursFromDiffDate(diffDate) {
-    return Math.floor(diffDate % (24 * 60 * 60 * 1000) / (1000 * 60 * 60))
-}
+const ExtractHoursFromDiffDate = function (diffDate) {
+  return Math.floor((diffDate % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
+};
 
 function ExtractMinutesFromDiffDate(diffDate) {
-    return Math.floor(diffDate % (60 * 60 * 1000) / (1000 * 60))
+  return Math.floor((diffDate % (60 * 60 * 1000)) / (1000 * 60));
 }
 
-function ExtractSecundesMinutesFromDiffDate(diffDate) {
-    return Math.floor(diffDate % (60 * 1000) / (1000))
+function ExtractSecondsFromDiffDate(diffDate) {
+  return Math.floor((diffDate % (60 * 1000)) / 1000);
 }
 
-setInterval(() => {
-    daysCounterElement.innerHTML = ExtractDaysFromDiffDate(GetDiffDate(startDate));
-    hoursCounterElement.innerHTML = ExtractHoursFromDiffDate(GetDiffDate(startDate));
-    minutesCounterElement.innerHTML = ExtractMinutesFromDiffDate(GetDiffDate(startDate));
-    secondsCounterElement.innerHTML = ExtractSecundesMinutesFromDiffDate(GetDiffDate(startDate));
-    // console.log(GetDiffDate(startDate))
-}, 1000);
+if (ExtractSecondsFromDiffDate(GetDiffDate(startDate)) <=0) {
+  var parent = document.getElementById("counter-wrapper");
+  var child = document.getElementById("counter-inner");
+  parent.removeChild(child);
+  
 
+  console.log("курс уже начался")
+} else {
 
-// console.log(daysCounterElement, hoursCounterElement, minutesCounterElement, secondsCounterElement)
+  setInterval(() => {
+    daysCounterElement.innerHTML = ExtractDaysFromDiffDate(
+        GetDiffDate(startDate)
+    );
+    hoursCounterElement.innerHTML = ExtractHoursFromDiffDate(
+        GetDiffDate(startDate)
+    );
+    minutesCounterElement.innerHTML = ExtractMinutesFromDiffDate(
+        GetDiffDate(startDate)
+    );
+    secondsCounterElement.innerHTML = ExtractSecondsFromDiffDate(
+        GetDiffDate(startDate)
+    );
+  }, 1000);
+}
